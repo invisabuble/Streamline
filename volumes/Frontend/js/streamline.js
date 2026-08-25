6fbcaa752f78863f3520970dc5acb7849e9b6f6b
Luke Park <luke.matthew.park@hotmail.co.uk>
2026-08-25 16:17:37 +0100

    target selector

--- volumes/Frontend/js/streamline.js
+++ volumes/Frontend/js/streamline.js
@@ -3,9 +3,32 @@ console.log("Streamline.js loaded!");
 const host = "http://streamline.local:8080"
 
 // Get the list of devices from the backend.
-async function get_devices() {
-    const devices = await fetch(`${host}/api/devices`);
-    return devices
+async function get_route(route) {
+    const response = await fetch(`${host}/api/${route}`);
+    return await response.json()
 }
 
-console.log(get_devices());
+// Main streamline method.
+async function streamline () {
+
+    // Get the devices and files from the API.
+    const devices = await get_route("devices");
+    const files   = await get_route("get_file_list");
+
+    const selector = document.getElementById("devices");
+
+    for (const [ip, device] of Object.entries(devices)) {
+        const option = document.createElement("option");
+        option.value = ip;
+        option.textContent = device.device_info.friendlyName || ip;
+        selector.appendChild(option);
+    }
+
+    for (const [entry, info] of Object.entries(files)) {
+        console.log(entry, info);
+    }
+
+}
+
+streamline();
+
